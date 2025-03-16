d716d3f331e74108f8b5dae8996aeb36b2eb7b81
Adam Liberov <adamsholet@gmail.com>
2025-03-16 19:32:12 +0200

    inital commit

---
+++ main.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+// Swiss Knife that lets you decide what kind attack you want to perform For example:
+// 1 - PERS - COM Hijacking, EVASION - HELLHALL, INJECTION - PROCESS DOPPLEGANGING....
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
